bcc68c37ce66f2eca11990f1621ca28a7afb1e6f
Iryna-Somyuk <Iryna.Somuyk@gmail.com>
2022-12-07 21:41:26 +0200

    commit css

--- src/fetctSubmit.js
+++ src/fetctSubmit.js
@@ -3,78 +3,45 @@ import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 export default class apiService {
-    constructor(){
-        this.searchQuery = "";
-        this.page = 1;
-
-    }
-    async getUser1() {
-        try {
-          
-          const response = await axios.get('https://pixabay.com/api/', {
-            params: {
-              key: "31842853-978e72f35827287bca0aab5ce",
-              q: `${this.searchQuery}`,
-              image_type: "photo",
-              orientation: "horizontal",
-              safesearch: "true",
-              page: `${this.page}`,
-              per_page: 40,
-            }
-          });
-          if (response.data.hits.length === 0) {
-            Notify.failure("Sorry, there are no images matching your search query. Please try again.");            
-        } else if (this.page === 1) {
-            Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
-        }            
-
-
-
-          this.page += 1;
-         return response.data;
-      
-          //console.log(data);
-        //   listPhoto.insertAdjacentHTML('beforeend', createMarkupGallery(data.hits));
-        }catch (error) {
-          Notify.failure(error.message);
-        }
-      }
-  
-      resetPage(){
-        this.page = 1;
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+  async getRequest() {
+    try {
+      const response = await axios.get('https://pixabay.com/api/', {
+        params: {
+          key: '31842853-978e72f35827287bca0aab5ce',
+          q: `${this.searchQuery}`,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: 'true',
+          page: `${this.page}`,
+          per_page: 40,
+        },
+      });
+      if (response.data.hits.length === 0) {
+        Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+      } else if (this.page === 1) {
+        Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
       }
 
-
-      get query() {
-        return this.searchQuery;
-    };
-
-
-    set query(newQuery) {
-        this.searchQuery = newQuery;
+      this.page += 1;
+      return response.data;
+    } catch (error) {
+      Notify.failure(error.message);
     }
- 
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  // get query() {
+  //   return this.searchQuery;
+  // }
+
+  // set query(newQuery) {
+  //   this.searchQuery = newQuery;
+  // }
 }
-
-
-// const listPhoto = document.querySelector('.gallery');
-
-//   function createMarkupGallery(arr){
-//     return arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `<div class="photo-card">
-//      <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" class="picture"  /></a>
-//       <div class="info">
-//         <p class="info-item">
-//           <b>Likes</b> ${likes}
-//         </p>
-//         <p class="info-item">
-//           <b>Views</b> ${views}
-//         </p>
-//         <p class="info-item">
-//           <b>Comments</b> ${comments}
-//         </p>
-//         <p class="info-item">
-//           <b>Downloads</b> ${downloads}
-//         </p>
-//       </div>
-//     </div>`).join('')
-//   }
--- src/index.js
+++ src/index.js
@@ -1,81 +1,107 @@
 import './css/styles.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import apiService from './fetctSubmit.js';
 
-
-// const refs = {
-//     form: document.querySelector(".search-form"),
-//     input: document.querySelector("[name=searchQuery]"),
-//     formBtn: document.querySelector("button"),
-//     gallery: document.querySelector(".gallery"),
-//     loadMoreBtn: document.querySelector(".load-more"),  
-//   }
-
-
-//const BASE_URL = 'https://pixabay.com/api/';
-//refs.input.addEventListener('input', getUser());
-
-//  axios.get('${BASE_URL}', {
-//         params: {
-//             key: "31842853-978e72f35827287bca0aab5ce",
-// 	            q: `yellow+flowers`,
-// 	            image_type: "photo",
-// 	            orientation: "horizontal",
-// 	            safesearch: "true",
-//         }
-//       })
-//       .then((response) => {console.log(response.data)})
-//       .catch ((error) => {
-//       console.error(error)
-//     });
-
-// async function getUser(){
-//     const resp = await fetch('https://pixabay.com/api/?key=31842853-978e72f35827287bca0aab5ce&q=yellow+flowers&image_type=photo&orientation=horizontal&safesearch=true');
-//               console.log(resp);
-//    const pic = await resp.json();
-//    console.log(pic);           
-// }
-// getUser();
-//const listPhoto = document.querySelector('.gallery');
-const form = document.querySelector(".search-form");
-
-const searchBtn = document.querySelector("button");
-const loadBtn = document.querySelector('.load-more')
+const form = document.querySelector('.search-form');
+const loadBtn = document.querySelector('.load-more');
+const listPhoto = document.querySelector('.gallery');
 
 loadBtn.addEventListener('click', onLoad);
 form.addEventListener('submit', onSearch);
 
+let counter = 0;
+loadBtn.hidden = true;
+
 const ApiService = new apiService();
 
-//loadBtn.hidden = true;
-function onSearch(evn) {
-  evn.preventDefault();
-  ApiService.query = evn.currentTarget.elements.searchQuery.value;
-  console.log(ApiService.query);
-ApiService.resetPage();
-ApiService.getUser1().then((data) => {
-  console.log(data)
-});
-
-
-
+function onClear() {
+  listPhoto.innerHTML = '';
 }
 
-function onLoad(){
-  ApiService.getUser1();
+function onSearch(evn) {
+  evn.preventDefault();
+  ApiService.searchQuery = evn.currentTarget.elements.searchQuery.value;
+  console.log(ApiService.searchQuery);
+  if (ApiService.searchQuery === '') {
+    return Notify.failure('Sorry, there are no images matching your search query. Please enter something!'
+    );
+  };
+  ApiService.resetPage();
+  ApiService.getRequest().then(data => {
+    console.log(data);
+    onClear();
+    createMarkupGallery(data);
+    loadBtn.hidden = false;
+  });
+}
+
+function onLoad() {
+  ApiService.getRequest().then(createMarkupGallery);
+}
+
+function createMarkupGallery(data) {
+  const markupGallery = data.hits
+    .map(({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<div class="photo-card">
+     <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" class="picture"  /></a>
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b> ${likes}
+        </p>
+        <p class="info-item">
+          <b>Views</b> ${views}
+        </p>
+        <p class="info-item">
+          <b>Comments</b> ${comments}
+        </p>
+        <p class="info-item">
+          <b>Downloads</b> ${downloads}
+        </p>
+      </div>
+    </div>`
+    )
+    .join('');
+
+  listPhoto.insertAdjacentHTML('beforeend', markupGallery);
+  counterImg(data);
+
+  new SimpleLightbox('.gallery a', {
+    captionPosition: 'bottom',
+    captionsData: 'alt',
+    captionDelay: 250,
+  }).refresh();
+}
+
+function counterImg(data) {
+  counter += data.hits.length;
+  if (counter > 40) {
+    console.log(counter);
+    smoothScroll();
+  } else if (counter >= data.totalHits) {
+    loadBtn.hidden = true;
+    return Notify.failure("We're sorry, but you've reached the end of search results.");
   }
-  
+}
 
+function smoothScroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
 
-
-
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
 
 //31842853-978e72f35827287bca0aab5ce
-// {
-//   params: {
-//       key: "31842853-978e72f35827287bca0aab5ce",
-//         q: `yellow+flowers`,
-//         image_type: "photo",
-//         orientation: "horizontal",
-//         safesearch: "true",
-//   }
-// }
+
